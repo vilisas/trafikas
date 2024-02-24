b1b6ec28f843d2a7e9fe745818832cb258b08712
Vilius LY3FF <vilisas@gmail.com>
2024-02-24 11:08:40 +0200

    pridetas laiko offset'as, tag'as

--- misc/csv_to_line.py
+++ misc/csv_to_line.py
@@ -5,6 +5,8 @@ from influx_line_protocol import Metric
 import csv
 import datetime
 
+time_offset = 25200 # 7 valandos sekundemis (laikom, kad pradzia 7 valanda UTC)
+
 with open('aktyvumas.csv', newline='', encoding='utf-8') as f:
     reader = csv.reader(f)
     for row in reader:
@@ -13,13 +15,14 @@ with open('aktyvumas.csv', newline='', encoding='utf-8') as f:
         epoch=0
         valid=False
         try:
-            epoch_ns = int(datetime.datetime.strptime(date, "%Y-%m-%d").timestamp()) * 1000000000
+            epoch_ns = int(datetime.datetime.strptime(date, "%Y-%m-%d").timestamp() + time_offset) * 1000000000
             count=int(row[1])
             valid=True
         except:
             pass
         if (valid):
             metric = Metric("trafikas")
+            metric.add_tag("data", "count")
             metric.with_timestamp(epoch_ns)
             metric.add_value('count', count)
             print(metric)
